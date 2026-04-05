f2b007c25a41d187882d6698dc150e0992d6b7ff
Benyamin206 <benyaminsibarani2406@gmail.com>
2026-04-05 15:44:21 +0700

    revision init

--- app1/HelloWorld.go
+++ app1/HelloWorld.go
@@ -1,9 +1,17 @@
 package app1
 
 func HelloWorld(name string) string {
-	return "Hiy " + name
+	return "Hi " + name
 }
 
 func HelloWorldBen(name string) string {
 	return "Hi Ben " + name
+}
+
+func HelloWorldName(name string) string {
+	return name
+}
+
+func HelloCandra(name string) string {
+	return name
 }
--- app1/HelloWorld_test.go
+++ app1/HelloWorld_test.go
@@ -2,25 +2,49 @@ package app1
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestHelloWorld(t *testing.T) {
-	value := HelloWorld("Andi")
+// func TestHelloWorld(t *testing.T) {
+// 	value := HelloWorld("Andi")
 
-	if value != "Hi Andi" {
-		t.Error("Result must be Hi Andi but the value is " + value)
-	}
+// 	if value != "Hi Andi" {
+// 		t.Error("Result must be Hi Andi but the value is " + value)
+// 	}
 
-	fmt.Println("Testing Done")
-}
+// 	fmt.Println("Testing Done")
+// }
 
 func TestHelloWorldBen(t *testing.T) {
 	value := HelloWorldBen("Sbrn")
-	if value != "Hi Ben Sbrn" {
-		t.Fatal("The value is " + value + " but the expected is Hi Ben Sbrn")
+	assert.Equal(t, "Hi Ben Sbrn", value, "Should be same")
+	fmt.Println("Testing 1 Selesai")
+}
+
+func TestHelloCandraRequire(t *testing.T) {
+	value := HelloCandra("Candra")
+
+	require.Equal(t, "Candras", value, "Should be equal")
+	fmt.Println("Selesaiii")
+}
+
+func TestHelloWorldName(t *testing.T) {
+	value := HelloWorldName("Eko")
+
+	assert.Equal(t, "Eko", value, "they should be equal")
+
+	fmt.Println("Testing 2 Selesai")
+}
+
+func SkipTest(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Test tidak dapat berjalan di windows")
 	}
-	fmt.Println("Testing Done")
+	fmt.Println("Doneeee")
 }
 
 // fail => menggagalkan unit test tetapi tetap melanjutkan program selanjutnya
